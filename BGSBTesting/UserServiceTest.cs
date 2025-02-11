3e73a1edbe6b512ffa1f2b3e90cd75ae88006b9e
NailMinnigalin <DisappearedCat@yandex.ru>
2025-02-11 05:51:20 +0300

    Add RegisterUserAsyncValidInputRegistersUserSuccessfully test

---
+++ BGSBTesting/UserServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using BoardGameSchedulerBackend.BusinessLayer;
+using Moq;
+
+namespace BGSBTesting
+{
+	[TestClass]
+	public class UserServiceTest
+	{
+		private Mock<IUserRepository> _iUserRepositoryMock = default!;
+		private UserService _userService = default!;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			_iUserRepositoryMock = new Mock<IUserRepository>();
+			_userService = new UserService(_iUserRepositoryMock.Object);
+		}
+
+		[TestMethod]
+		public async Task RegisterUserAsyncValidInputRegistersUserSuccessfully()
+		{
+			_iUserRepositoryMock
+				.Setup(iur => iur.CreateAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(Task.CompletedTask);
+
+			await _userService.RegisterUserAsync("validUserName", "validUserEmail@example.com", "ValidPassword");
+		}
+	}
+}
